e9052f658d2fecdeac21342c9da14ba8943626fa
tkorvel <korveltanel@gmail.com>
2022-01-23 02:14:06 +0200

    [init] - first commit - project initialization

---
+++ src/main/java/com/sda/javaee9spring/Javaee9SpringApplication.java
@@ -0,0 +1,14 @@
+package com.sda.javaee9spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaee9SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaee9SpringApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/sda/javaee9spring/Javaee9SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javaee9spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaee9SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
