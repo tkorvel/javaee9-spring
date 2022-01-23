8e4c09e64b778c4964faeb252cd63717c6f21f6e
tkorvel <korveltanel@gmail.com>
2022-01-23 08:39:55 +0200

    [add] - our first controller:)

---
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -0,0 +1,24 @@
+package com.sda.javaee9spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class FirstController {
+
+// the same as @Slf4j
+// private static final Logger log = LoggerFactory.getLogger(FirstController.class);
+
+
+
+    @GetMapping("/my-first-page")
+    public String firstPage() {
+        log.info("firstPage() method called!!!");
+        // looking for html page inside resources/templates
+        return "home-page";
+    }
+}
