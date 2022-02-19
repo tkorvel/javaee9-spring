c39c0872b43721264fd0a0534803bb67fe18cb01
tkorvel <korveltanel@gmail.com>
2022-02-19 08:38:06 +0200

    [add] - find person by id endpoint created

--- src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
@@ -5,6 +5,7 @@ import com.sda.javaee9spring.service.RealPersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +16,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class PersonRestController {
 
-    private final RealPersonService  personService;
+    private final RealPersonService personService;
 
     @Autowired
     public PersonRestController(RealPersonService personService) {
         this.personService = personService;
     }
+
     @GetMapping("/persons")
     public List<PersonEntity> findAllPersons() {
         log.info("findALlPersons");
 
         return personService.readAllPersonEntities();
+    }
+
+    // /persons/1
+    // /persons/10
+    // /persons/12345
+    @GetMapping("/persons/{id}")
+    public PersonEntity findPersonEntityById(@PathVariable("id") Long personId) {
+        log.info("trying to find person entity by id:[{}]", personId);
+
+        return personService.readPersonEntityById(personId);
+
     }
 }
--- src/main/java/com/sda/javaee9spring/service/RealPersonService.java
+++ src/main/java/com/sda/javaee9spring/service/RealPersonService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -24,5 +25,17 @@ public class RealPersonService {
         log.info("persons entities read from db: {}", result);
 
         return result;
+    }
+
+    public PersonEntity readPersonEntityById(Long id) {
+        log.info("read entity by id: [{}]", id);
+
+        Optional<PersonEntity> maybePerson = personRepository.findById(id);
+        PersonEntity result = null;
+        if (maybePerson.isPresent()) {
+            result = maybePerson.get();
+        }
+        log.info("found Person entity: [{}]", result);
+         return result;
     }
 }
